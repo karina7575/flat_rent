a3ccd3da6abc8675a124331e25180ccaf0bbe019
Karina <[karinakios@mail.ru]>
2025-05-09 13:45:53 +0300

    chapter 1 done

--- flat_rent/src/main/java/com/javaacademy/demo/Entity/Booking.java
+++ flat_rent/src/main/java/com/javaacademy/demo/Entity/Booking.java
@@ -1,8 +1,34 @@
 package com.javaacademy.demo.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 public class Booking {
-    //дата начала бронирования, дата окончания бронирования, ссылка на клиента, ссылка на объявление, стоимость всего бронирования.
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private LocalDateTime startBookingDate;     //дата начала бронирования
+
+    @Column(nullable = false)
+    private LocalDateTime finishBookingDate;    //дата окончания бронирования
+
+    @Column(nullable = false)
+    private Client client;                      //ссылка на клиента
+
+    @Column(nullable = false)
+    private Advert advert;                      //ссылка на объявление
+
+    @Column(nullable = false)
+    private BigDecimal bookingPrice;            //стоимость всего бронирования
+
 }
